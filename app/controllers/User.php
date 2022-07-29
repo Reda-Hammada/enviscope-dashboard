67b8981b11b3a385d1dc937d3c44b854c46497d9
Reda Hammada <hammada.reda.dev@gmail.com>
2022-07-29 19:33:01 +0200

    login form validation done

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -2,18 +2,81 @@
 
 class User extends Controller {
     
+
+    public function __construct(){
+
+    }
    
     public function login(){
 
-      
-        $data = [
+     
+        //Check for GET
+        if(isset($_GET['login'])){
 
-            'username' =>'',
-            'password'=> ''
 
-        ];
+    
+            // Process form     
+            // sanatize data
+            $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
 
+            //Init Data
+               $data = [
+
+                'username' =>$_GET['username'],
+                'username_err'=>'',
+                'password'=> $_GET['password'],
+                'password_err' =>''
+    
+            ];
+
+            
+
+            //Validate Email 
+
+            if(empty($data['username'])):
+                $data['username_err'] = 'Veuillez entrer le nom d’utilisateur';
+
+            endif;
+
+
+            //Validate password 
+
+            if(empty($data['password'])):
+
+                $data['password_err'] = " Veuillez entrer le mot de pass";
+
+            endif;
+
+
+            if(empty($data['username_err']) && empty($data['password_err'])){
+
+            }
+            else {
+
+                $this->view('user/login', $data);
+            }
+
+
+            
+        }else{
+
+        //Init Data
+            $data = [
+
+                'username' =>'',
+                'username_err'=>'',
+                'password'=> '',
+                'password_err' =>''
+    
+            ];
+             
+        // Load view
         $this->view('User/login', $data);
+
+        }
+
+     
+
 
     }
 
@@ -25,6 +88,13 @@ class User extends Controller {
 
 
 
+    }
+
+    public function forget(){
+
+
+
+        $this->view('user/forget');
     }
     
 }
--- app/views/User/login.php
+++ app/views/User/login.php
@@ -1,7 +1,5 @@
 <?php 
-
 require_once '../app/views/inc/header.php';
-
 
 ?>
 
@@ -13,12 +11,19 @@ require_once '../app/views/inc/header.php';
     <img src="<?php echo URLROOT?>asset/images/logo.jpg"  alt="logo">
 </div>
 
-    <form class=' form form-group' method='GET'>
+    <form action='<?php echo URLROOT?>user/login' class=' form form-group' method='GET'>
+
+        <label class='label' for='name'>nom d'utilisateur :</label>
         <div class="username">
-            <input type='text' name="username">
+      
+            <input type='text' name="username" value=<?php echo $data['username'] ?>><br>
+            <span ><?php echo $data['username_err'] ?></span>
         </div>
+
+        <label  class='label' for='name'>mot de pass :</label>
         <div class="password">
-             <input type="password" name="password">
+             <inputtype="password" name="password" value=<?php echo $data['username_err'] ?>>
+
         </div>
         <div class='login'>
             
@@ -32,7 +37,5 @@ require_once '../app/views/inc/header.php';
 
 
 <?php
-
 require_once '../app/views/inc/footer.php';
-
 ?>
