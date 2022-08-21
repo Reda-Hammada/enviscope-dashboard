c0d458eb6709cd28e53c054df427e49950c45e87
Reda Hammada <hammada.reda.dev@gmail.com>
2022-08-22 00:45:09 +0200

    update

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -4,7 +4,8 @@ require_once '../app/views/inc/header.php';
 
 ?>
 
-   
+    <h1>MVC FRAMEWORK</h1>
+    <a href=<?php echo URLROOT .'user/login' ?>>Login </a>
 <?php
 
 require_once '../app/views/inc/footer.php';
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-require_once '../../app/bootstrap.php';
+require_once './app/bootstrap.php';
 
 // Init Core Library 
 
