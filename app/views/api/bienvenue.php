8e4d3db165250d7c21abf98b077066d7649dd418
Reda Hammada <hammada.reda.dev@gmail.com>
2022-08-22 15:46:13 +0200

    update

--- app/views/api/bienvenue.php
+++ app/views/api/bienvenue.php
@@ -3,4 +3,4 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header('Content-type:application/json');
 
-echo json_encode($data['bienvenue']);
+echo json_encode($data['bienvenue'], TRUE);
